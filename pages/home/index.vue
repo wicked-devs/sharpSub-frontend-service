f5b54449100505b7cc6c7b499b85b5266893f2df
legionite-scripts <samuelonwuka88@gmail.com>
2024-08-12 16:12:33 +0100

    Fixes

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,103 +1,54 @@
 <template>
   <NuxtLayout name="new">
     <main>
-      <div class="container mt-5">
-        <div class="text flexed-column">
-          <h1 class="weight-6 mb-2">
-            Effortless Virtual Top-Up for All Networks
-          </h1>
-          <p class="mb-2 weight-4">
-            Whether you need to pay bills, top up airtime, or purchase data, we
-            make it easy for everyone.
-          </p>
-          <form ref="box">
-            <input type="email" placeholder="Enter email address" />
-            <button class="btn-primary" @click.prevent>Get Free Access</button>
-          </form>
-          <!-- <div class="stats">
-            <div>
-              <h2>2943</h2>
-              <p>Accounts Activated</p>
-            </div>
-            <div>
-              <h2>$1M+</h2>
-              <p>Transactions Completed</p>
-            </div>
-          </div> -->
-        </div>
+      <section class="text">
+        <h1 class="weight-7 mb-3">
+          Effortless Virtual Top-Up for All Networks
+        </h1>
+        <p class="mb-3">
+          Effortless Virtual Top-Up for All Networks Whether you need to pay
+          bills, top up airtime, or purchase data, we make it easy for everyone.
+        </p>
 
-        <img src="@/assets/images/hero.webp" alt="" />
-      </div>
+        <form class="email">
+          <input type="email" placeholder="Enter Email Address" />
+          <button class="btn-primary">Get Free Access</button>
+        </form>
+      </section>
+
+      <img src="@/assets/images/hero.webp" alt="Hero" class="center" />
     </main>
   </NuxtLayout>
 </template>
 
-<script setup>
-// import { gsap } from "gsap";
-// import { ref, onMounted } from "vue";
-// const box = ref(null);
-
-// const { $gsap } = useNuxtApp();
-
-// onMounted(() => {
-//   // Zoom in and out animation
-//   gsap.to(box.value, {
-//     scale: 1.1,      // Zoom in to 1.5x the original size
-//     duration: 1.5,   // Duration of the zoom in
-//     ease: 'power1.inOut', // Easing function for the zoom
-//     yoyo: true,      // Reverse the animation (zoom out)
-//     repeat: 0,      // Repeat infinitely
-//   });
-// });
-</script>
+<script setup></script>
 
 <style scoped>
-.container {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 20px;
-}
-.text {
-  justify-content: center;
-}
-.text p {
-  color: #8a8a8a;
-}
-form {
+main {
   display: flex;
- justify-content: space-between;
-  border: 1px solid #8a8b8b;
-  padding: 5px !important;
-  border-radius: 8px;
-  width:fit-content !important;
-  background: #111010;
+  flex-direction: row;
+  justify-content: space-between !important;
+  flex-wrap: wrap;
+  gap: 50px;
+  border: 1px solid #333;
+}
+main section {
+  width: 50%;
+  border: 1px solid #fff;
 }
 
-form input {
-  background: none !important;
-}
-form input:focus {
-  outline: none;
-}
-.container img {
-  grid-column: 4 !important;
-}
-
-@media (max-width: 1000px) {
-  .container {
-    grid-template-columns: 1fr !important;
+@media (max-width: 768px) {
+  img {
+    width: 100% !important;
   }
-  .container img {
-    grid-column: auto !important;
+}
+
+@media (max-width: 1300px) {
+  main section {
     width: 100%;
   }
-}
-@media (max-width: 1200px) {
-}
-@media(max-width:768px){
-  form{
-    width:100% !important;
+  img {
+    height: 10% !important;
   }
 }
-
 </style>
